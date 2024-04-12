4b3618e842bf409b714464e4d187fd468b16e7d2
rendai okada <okada@hatchout.net>
2024-04-12 16:31:30 +0900

    section2-10まで完了

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_aws_calendar/model/schedule.dart';
 import 'package:intl/intl.dart';
 
 class CalendarView extends StatefulWidget {
@@ -16,17 +17,38 @@ class _CalendarViewState extends State<CalendarView> {
   late int initialIndex;
   int monthDuration = 0;
 
+  Map<DateTime, List<Schedule>> scheduleMap = {
+    DateTime(2024, 4, 11): [
+      Schedule(
+        title: '買い出し',
+        startAt: DateTime(2024, 4, 11, 10),
+        endAt: DateTime(2024, 4, 11, 12),
+      ),
+      Schedule(
+        title: '宿題をする',
+        startAt: DateTime(2024, 4, 11, 15),
+        endAt: DateTime(2024, 4, 11, 17),
+      ),
+    ],
+    DateTime(2024, 4, 12): [
+      Schedule(
+        title: '買い出し',
+        startAt: DateTime(2024, 4, 12, 10),
+        endAt: DateTime(2024, 4, 12, 12),
+      ),
+    ]
+  };
+
   @override
   void initState() {
     super.initState();
 
-    initialIndex = (now.year - firstDay.year) * 12 + (now.month - firstDay.month);
+    initialIndex =
+        (now.year - firstDay.year) * 12 + (now.month - firstDay.month);
     controller = PageController(initialPage: initialIndex);
     controller.addListener(() {
       monthDuration = (controller.page! - initialIndex).round();
-      setState(() {
-
-      });
+      setState(() {});
     });
   }
 
@@ -34,11 +56,10 @@ class _CalendarViewState extends State<CalendarView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme
-            .of(context)
-            .primaryColor,
-        title: Text(DateFormat('yyyy年 M月').format(
-            DateTime(now.year, now.month + monthDuration)),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(
+            DateFormat('yyyy年 M月')
+                .format(DateTime(now.year, now.month + monthDuration)),
             style: const TextStyle(color: Colors.white)),
         elevation: 0,
       ),
@@ -46,14 +67,11 @@ class _CalendarViewState extends State<CalendarView> {
         children: [
           Container(
             height: 30,
-            color: Theme
-                .of(context)
-                .primaryColor,
+            color: Theme.of(context).primaryColor,
             child: Row(
               children: weekName
-                  .map((e) =>
-                  Expanded(
-                      child: Container(
+                  .map((e) => Expanded(
+                          child: Container(
                         alignment: Alignment.center,
                         child: Text(
                           e,
@@ -76,7 +94,8 @@ class _CalendarViewState extends State<CalendarView> {
           List<Widget> _list = [];
           List<Widget> _listCache = [];
 
-          DateTime date = DateTime(now.year, now.month + index - initialIndex, 1);
+          DateTime date =
+              DateTime(now.year, now.month + index - initialIndex, 1);
           int monthLastDay = DateTime(date.year, date.month + 1, 1)
               .subtract(const Duration(days: 1))
               .day;
@@ -86,21 +105,18 @@ class _CalendarViewState extends State<CalendarView> {
               day: i + 1,
               now: now,
               cacheDate: DateTime(date.year, date.month, i + 1),
+              scheduleList: scheduleMap[DateTime(date.year, date.month, i + 1)],
             ));
             int repeatNumber = 7 - _listCache.length;
-            if (date
-                .add(Duration(days: i))
-                .weekday == 7) {
+            if (date.add(Duration(days: i)).weekday == 7) {
               if (i < 7) {
                 _listCache.insertAll(
                   0,
                   List.generate(
                     repeatNumber,
-                        (index) =>
-                        Expanded(
-                          child: Container(
-                              color: Colors.black.withOpacity(0.1)),
-                        ),
+                    (index) => Expanded(
+                      child: Container(color: Colors.black.withOpacity(0.1)),
+                    ),
                   ),
                 );
               }
@@ -111,12 +127,11 @@ class _CalendarViewState extends State<CalendarView> {
               _listCache.addAll(
                 List.generate(
                   repeatNumber,
-                      (index) =>
-                      Expanded(
-                        child: Container(
-                          color: Colors.black.withOpacity(0.1),
-                        ),
-                      ),
+                  (index) => Expanded(
+                    child: Container(
+                      color: Colors.black.withOpacity(0.1),
+                    ),
+                  ),
                 ),
               );
               _list.add(Row(
@@ -128,8 +143,7 @@ class _CalendarViewState extends State<CalendarView> {
           return Column(
             children: _list,
           );
-        }
-    );
+        });
   }
 }
 
@@ -137,38 +151,64 @@ class _CalendarItem extends StatelessWidget {
   final int day;
   final DateTime now;
   final DateTime cacheDate;
+  final List<Schedule>? scheduleList;
 
   const _CalendarItem(
-      {required this.day, required this.now, required this.cacheDate, Key? key})
+      {required this.day,
+      required this.now,
+      required this.cacheDate,
+      this.scheduleList,
+      Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     bool isToday =
-        (now
-            .difference(cacheDate)
-            .inDays == 0) && (now.day == cacheDate.day);
+        (now.difference(cacheDate).inDays == 0) && (now.day == cacheDate.day);
     return Expanded(
       child: Container(
-        alignment: Alignment.topLeft,
         height: 80,
         decoration: BoxDecoration(
           border: Border.all(color: Colors.grey),
         ),
-        child: Container(
-            width: 20,
-            height: 20,
-            color: isToday
-                ? Theme
-                .of(context)
-                .primaryColor
-                .withOpacity(0.8)
-                : null,
-            alignment: Alignment.center,
-            child: Text(
-              '$day',
-              style: TextStyle(color: isToday ? Colors.white : null),
-            )),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 20,
+              height: 20,
+              color: isToday
+                  ? Theme.of(context).primaryColor.withOpacity(0.8)
+                  : null,
+              alignment: Alignment.center,
+              child: Text(
+                '$day',
+                style: TextStyle(color: isToday ? Colors.white : null),
+              ),
+            ),
+            scheduleList == null
+                ? Container()
+                : Column(
+                    children: scheduleList!
+                        .map((e) => Container(
+                              width: double.infinity,
+                              height: 20,
+                              alignment: Alignment.centerLeft,
+                              margin: const EdgeInsets.only(left: 2, right: 2, top: 2),
+                              padding: const EdgeInsets.only(left: 2, right: 2),
+                              color: Theme.of(context)
+                                  .primaryColor
+                                  .withOpacity(0.8),
+                              child: Text(
+                                e.title,
+                                style: const TextStyle(color: Colors.white, fontSize: 10),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ))
+                        .toList(),
+                  )
+          ],
+        ),
       ),
     );
   }
